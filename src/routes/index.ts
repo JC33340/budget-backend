86ee92eb998289053e16045084c4561f07ea0734
Jason Chan <jasonchanhw0531@gmail.com>
2025-03-20 23:47:53 +0000

    revert changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import authRoutes from './auth/index';
 import transactionRoutes from './transactions/index';
 import homePageRoutes from './homePage/index';
-import { wakedb } from '../controllers/database/database.controllers';
 
 const router = Router();
 
@@ -11,7 +10,5 @@ router.use('/auth', authRoutes);
 router.use('/transactions', transactionRoutes);
 
 router.use('/homepage', homePageRoutes);
-
-router.get('/wakedb', wakedb);
 
 export default router;
